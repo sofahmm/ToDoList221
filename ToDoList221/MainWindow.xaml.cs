d19ae5a6adbef8a12b123a8a030e721b88e118d9
София Кумышбаева <kimsofocka10@gmail.com>
2025-04-15 16:48:38 +0300

    Добавьте файлы проекта.

---
+++ ToDoList221/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoList221
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<TodoItem> Tasks { get; set; } = new ObservableCollection<TodoItem>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            TaskListLb.ItemsSource = Tasks;
+        }
+
+        private void AddTaskBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TaskInputTb.Text))
+            {
+                Tasks.Add(new TodoItem { Title = TaskInputTb.Text, IsDone = false });
+                TaskInputTb.Text = string.Empty;
+            }
+        }
+
+        private void DeleteTaskBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void CheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
